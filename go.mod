2b0ba646cd99eda714c561288e29fbf9cad5dfc9
vearne <asdwoshiaotian@gmail.com>
2024-04-25 11:15:10 +0800

    add xpath test

--- go.mod
+++ go.mod
@@ -8,8 +8,15 @@ require (
 )
 
 require (
+	github.com/antchfx/jsonquery v1.3.4 // indirect
+	github.com/antchfx/xpath v1.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/rule/rule_test.go
@@ -0,0 +1,96 @@
+package rule
+
+import (
+	"fmt"
+	//"fmt"
+	"github.com/antchfx/jsonquery"
+	"github.com/stretchr/testify/assert"
+
+	"strings"
+	"testing"
+)
+
+const json1 = `[
+  {
+    "id": 1,
+    "title": "The Go Programming Language",
+    "author": "Alan A. A. Donovan and Brian W. Kernighan"
+  },
+  {
+    "id": 2,
+    "title": "Effective Go",
+    "author": "The Go Authors"
+  }
+]`
+const json2 = `
+{
+	"person": {
+		"name": "John",
+		"age": 31,
+		"female": false,
+		"city": null,
+		"hobbies": [
+			"coding",
+			"eating",
+			"football"
+		]
+	}
+}`
+
+func TestXpathFind(t *testing.T) {
+	cases := []struct {
+		xpath    string
+		expected any
+	}{
+		{"//title", "Effective Go"},
+		{"//id", 2},
+	}
+	for _, item := range cases {
+		doc, _ := jsonquery.Parse(strings.NewReader(json1))
+		nodes := jsonquery.Find(doc, item.xpath)
+		exist := false
+		for _, node := range nodes {
+			if convStr(item.expected) == fmt.Sprintf("%v", node.Value()) {
+				exist = true
+			}
+		}
+		assert.True(t, exist)
+	}
+}
+
+func TestXpathFindOne(t *testing.T) {
+	cases := []struct {
+		xpath    string
+		expected any
+	}{
+		// Notice: the first element is 1, not zero
+		{"(//title)[2]", "Effective Go"},
+		{"(//id)[1]", 1},
+	}
+	for _, item := range cases {
+		doc, _ := jsonquery.Parse(strings.NewReader(json1))
+		node := jsonquery.FindOne(doc, item.xpath)
+		assert.Equal(t, convStr(item.expected), convStr(node.Value()))
+	}
+}
+
+func TestXpathFindOne2(t *testing.T) {
+	cases := []struct {
+		xpath    string
+		expected any
+	}{
+		// Notice: the first element is 1, not zero
+		{"/person/name", "John"},
+		{"/person/female", false},
+		{"/person/hobbies", []string{"coding", "eating", "football"}},
+	}
+	for _, item := range cases {
+		doc, _ := jsonquery.Parse(strings.NewReader(json2))
+		node := jsonquery.FindOne(doc, item.xpath)
+		assert.Equal(t, convStr(item.expected), convStr(node.Value()))
+	}
+}
+
+func convStr(v any) string {
+	return fmt.Sprintf("%v", v)
+}
